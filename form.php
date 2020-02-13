a374f891ad144a621aeb7574116a728b5b439f2f
Candace <2100978@talnet.nl>
2020-02-13 20:10:40 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bit Academy</title>
+</head>
+
+<body>
+
+    <input type="text" id="name" value="Voer je e-mailadres in">
+    <input type="button" value="Verstuur">
+
+</body>
+
+</html>
